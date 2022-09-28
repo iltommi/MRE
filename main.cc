94bbb588a9b70de686021f76771400015454d791
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2022-09-28 17:29:24 +0200

    1st commit

---
+++ main.cc
@@ -0,0 +1,6 @@
+#include "nApp.h"
+
+int main(int argc, char **argv) {
+    myApp my_app(argc,argv);
+    return my_app.exec();
+}
---
+++ my_app.h
@@ -0,0 +1,40 @@
+#ifndef nApp_H
+#define nApp_H
+
+#include <QApplication>
+#include <QVector>
+#include <QMainWindow>
+
+class my_class : public QWidget {
+    Q_OBJECT
+
+public:
+    my_class(QWidget *parent): QMainWindow(parent) {};
+    ~my_class() {};
+};
+
+
+class my_app : public QApplication {
+    Q_OBJECT
+public:
+    my_app( int &argc, char **argv ): QApplication(argc, argv) {};
+
+public slots:
+
+    QVector<my_class*> neus() {
+		QVector<my_class*> retList;
+		foreach (QWidget *widget, topLevelWidgets()) {
+			my_class *my_neu=qobject_cast<my_class *>(widget);
+			if (my_neu) retList << my_neu;
+		}
+		return retList;
+    };
+};
+
+Q_DECLARE_METATYPE(my_class*);
+Q_DECLARE_METATYPE(QVector<my_class*>);
+
+
+
+#endif
+
